2fb11cffb7a835fbe2bf366ae5fb44b9de26a58b
mr.simple <bboyfeiyu@gmail.com>
2015-03-20 13:30:06 +0800

    update

--- csdn.js
+++ csdn.js
@@ -1,1 +1,6 @@
-document.write("<h1>Hello World!</h1>")
+$(function(){
+var worlds = $("转载请标明出处：<a href=\"" + window.location.href + "\" target=\"_blank\">" + window.location.href + "</a>，本文出自：【<a href=\"http://blog.csdn.net/bboyfeiyu\">Mr.simple的博客</a>】");
+
+	$("#article_content").prepend(worlds);
+	console.log(123456789);
+})
